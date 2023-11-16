71d2d3ae5e37603e9383b2cee37fbf50c8e667dd
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-11-16 11:37:34 +0100

    Warning FLAC__HAS_OGG

--- src/flac_config.h
+++ src/flac_config.h
@@ -2,6 +2,7 @@
 #define PACKAGE_VERSION "1.3.4"
 #define HAVE_LROUND 1
 #define HAVE_STDINT_H 1
-#define FLAC__HAS_OGG 1
 #define FLAC__INTEGER_ONLY_LIBRARY 1
-#define FLAC__NO_ASM 1
+#ifndef FLAC__HAS_OGG
+#  define FLAC__HAS_OGG 1
+#endif
